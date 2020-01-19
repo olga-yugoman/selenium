c2bd161569cbb99e7ccd792257e2aa71c5219bf6
Olga Yugoman <o.yugoman@quality-lab.ru>
2020-01-19 21:35:23 +0200

    initial commit

---
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,40 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        wait.until(elementToBeClickable(By.name("btnK"))).click();
+        //driver.findElement(By.name("btnK")).click();
+        wait.until(titleContains("webdriver - Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
