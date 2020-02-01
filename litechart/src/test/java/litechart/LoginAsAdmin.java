95d04fc4b649dcf30b087a7f96d8bcb5d3001163
Olga Yugoman <o.yugoman@quality-lab.ru>
2020-02-01 19:11:49 +0200

    DZ6
    Run Firefox Nightly

--- litechart/src/test/java/litechart/LoginAsAdmin.java
+++ litechart/src/test/java/litechart/LoginAsAdmin.java
@@ -4,11 +4,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
@@ -19,7 +26,14 @@ public class LoginAsAdmin {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+
+        //запуск тестов в Firefox Nightly
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(new FirefoxBinary(new File("c:\\Program Files\\Firefox Nightly\\firefox.exe")));
+        driver = new FirefoxDriver(options);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+
         //driver = new FirefoxDriver();
         //driver = new InternetExplorerDriver();
         wait = new WebDriverWait(driver, 10);
